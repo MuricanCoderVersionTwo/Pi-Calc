cf3faeddfdeb1954c19e59c128f24460c1a1498e
Charles Schmitke <33550344+MuricanCoderVersionTwo@users.noreply.github.com>
2018-01-03 13:46:07 -0600

    Create pi1.py

---
+++ pi1.py
@@ -0,0 +1,12 @@
+f = open("RAM.txt")
+lines = f.readlines()
+for line in lines:
+    x = 1
+  pi = 0
+  while x <= piLen:
+    pi = pi + (4/x)
+    pi = pi - (4/(x+2))
+    x = x + 16
+  sys.stdout=open("pi.txt","a")
+  print("The time difference is", (now1 - now).total_seconds(), "seconds")
+  sys.stdout.close()
